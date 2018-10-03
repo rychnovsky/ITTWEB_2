71d64d90af5a2629565a276de0fa61676bc0cddd
Garrett Shirley <garrettjshirl@hotmail.com>
2018-10-03 17:01:43 +0200

    Files for user schema, user contoroller

---
+++ app_server/controllers/userController.js
@@ -0,0 +1,11 @@
+import user from '../models/user';
+
+let userController = {};
+
+userController.register = (req,res) => {
+
+}
+
+userController.login = (req,res) => {
+    
+}
---
+++ app_server/models/user.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema;
+
+//User Schema Definition
+const userSchema =  new Schema({
+
+})
+
+export default mongoose.model('user', userSchema);
