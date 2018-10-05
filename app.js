d9a12f7a9a9309b767007ca819d4ae776396a112
Vojtěch Rychnovský <rychnvoj@fel.cvut.cz>
2018-10-05 18:00:57 +0200

    Added CORS support

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ db.startConnection();
 
 var app = express();
 
+// allow CORS
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
